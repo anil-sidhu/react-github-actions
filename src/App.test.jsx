d9a0f7641500779be3af2229e3e9aecd05dc6fd9
Apple <Apple@MacBook-Pro.local>
2026-09-05 01:06:03 +0530

    test cases added for app comp

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -12,7 +12,7 @@ test("renders Code Step By Step heading", () => {
 
   const heading = screen.getByRole("heading", {
     level: 1,
-    name: /code step by step1/i,
+    name: /code step by step/i,
   });
 
   expect(heading).toBeInTheDocument();
